35c4b0952b50b24bc34f6a8e6b0ef0bf871f4dae
Bilshansakib <burakaghani@gmail.com>
2025-06-27 23:51:53 +0600

    first commit

---
+++ src/app/(nondashboard)/layout.tsx
@@ -0,0 +1,18 @@
+import Navbar from '@/components/Navbar';
+import { NAVBAR_HEIGHT } from '@/lib/constants';
+import React from 'react';
+function layout({ children }: { children: React.ReactNode }) {
+    return (
+        <div>
+            <Navbar/>
+             <main
+        className={`h-full flex w-full flex-col`}
+        style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}
+      >
+        {children}
+      </main>
+        </div>
+    );
+}
+
+export default layout;
