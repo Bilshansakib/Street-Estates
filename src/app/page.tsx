d70762df43866c1d289a0fd7ac68f6d0f3b35227
Bilshansakib <burakaghani@gmail.com>
2025-06-28 01:02:56 +0600

    update: navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div >
-      <h1 className="text-3xl sm:text-4xl font-bold">Street Estates</h1>
+      <h1 className="text-3xl sm:text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-primary-500 to-secondary-500">Street Estates</h1>
       
     </div>
   );
