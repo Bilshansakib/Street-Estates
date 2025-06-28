a8cf6e58394886846ae6b7ca86a168be949b8b11
Bilshansakib <burakaghani@gmail.com>
2025-06-28 23:57:25 +0600

    Update: Hero Section start

---
+++ src/app/(nondashboard)/landing/HeroSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeroSection = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white">
+            HeroSection
+        </div>
+    );
+};
+
+export default HeroSection;
