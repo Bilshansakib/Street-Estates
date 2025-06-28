af8a787255c253c96787c6b795babb701c7312a6
Bilshansakib <burakaghani@gmail.com>
2025-06-29 02:57:24 +0600

    Update: Feature section

--- src/app/(nondashboard)/landing/page.tsx
+++ src/app/(nondashboard)/landing/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import HeroSection from './HeroSection';
+import FeaturesSection from './FeaturesSection';
 
 const page = () => {
     return (
         <div>
-            Landing
+            <HeroSection />
+            <FeaturesSection />
         </div>
     );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Navbar from "@/components/Navbar";
+import Landing from "./(nondashboard)/landing/page";
 
 export default function Home() {
   return (
-    <div >
-      <h1 className="text-3xl sm:text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-primary-500 to-secondary-500">Street Estates</h1>
-      
+    <div className="h-full w-full">
+      <Navbar />
+      <main className={`h-full flex w-full flex-col`}>
+        <Landing />
+      </main>
     </div>
   );
 }
