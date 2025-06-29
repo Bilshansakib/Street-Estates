bfdb3ba9f7d0a651259e532c58cf939bc878f946
Bilshansakib <burakaghani@gmail.com>
2025-06-29 23:56:11 +0600

    update: Discover Section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,30 +32,28 @@ function Navbar() {
               </div>
             </div>
           </Link>
-         
         </div>
-          <p className="text-primary-200 hidden md:block">
-            Discover your perfect rental apartment with our advanced search
-          </p>
-          <div
-           className="flex items-center gap-5">
-            <Link href="/signin">
-                <Button
-                  variant="outline"
-                  className="text-white border-white bg-transparent hover:bg-white hover:text-primary-700 rounded-lg"
-                >
-                  Sign In
-                </Button>
-              </Link>
-              <Link href="/signup">
-                <Button
-                  variant="secondary"
-                  className="text-white bg-secondary-600 hover:bg-white hover:text-primary-700 rounded-lg"
-                >
-                  Sign Up
-                </Button>
-              </Link>
-           </div>
+        <p className="text-primary-200 hidden md:block">
+          Discover your perfect rental apartment with our advanced search
+        </p>
+        <div className="flex items-center gap-5">
+          <Link href="/signin">
+            <Button
+              variant="outline"
+              className="text-white border-white bg-transparent hover:bg-white hover:text-primary-700 rounded-lg"
+            >
+              Sign In
+            </Button>
+          </Link>
+          <Link href="/signup">
+            <Button
+              variant="secondary"
+              className="text-white bg-secondary-600 hover:bg-white hover:text-primary-700 rounded-lg"
+            >
+              Sign Up
+            </Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
