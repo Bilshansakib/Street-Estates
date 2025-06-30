9462b87117b3bd1d99518b113685840e7c045cb4
Bilshansakib <burakaghani@gmail.com>
2025-07-01 00:02:38 +0600

    update: footer section

--- src/app/(nondashboard)/landing/page.tsx
+++ src/app/(nondashboard)/landing/page.tsx
@@ -3,6 +3,7 @@ import HeroSection from './HeroSection';
 import FeaturesSection from './FeaturesSection';
 import DiscoverSection from './DiscoverSection';
 import CallToActionSection from './CallToActionSection';
+import FooterSection from './FooterSection';
 
 const page = () => {
     return (
@@ -11,6 +12,7 @@ const page = () => {
             <FeaturesSection />
             <DiscoverSection />
             <CallToActionSection />
+            <FooterSection />
         </div>
     );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,9 +25,9 @@ function Navbar() {
                 className="w-6 h-6"
               />
               <div className="text-xl font-bold">
-                RENT
+                Street
                 <span className="text-secondary-500 font-light hover:!text-primary-300">
-                  IFUL
+                  Estates
                 </span>
               </div>
             </div>
